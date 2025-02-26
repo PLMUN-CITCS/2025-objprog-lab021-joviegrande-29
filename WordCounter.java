e44d358e9d7b4c9813395863e73d8d794f85ab7a
joviegrande-29 <grandejovie_bscs@plmun.edu.ph>
2025-02-25 23:04:24 -0800

    Submitting OBJPROG Week 05 - Laboratory # 21

---
+++ WordCounter.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class WordCounter {
+    
+    public static String getSentenceInput() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a sentence: ");
+        String sentence = scanner.nextLine();
+        return sentence;
+    }
+
+
+    public static int countWords(String sentence) {
+ 
+        String[] words = sentence.trim().split("\\s+");
+        return words.length;
+    }
+
+    public static void main(String[] args) {
+
+        String sentence = getSentenceInput();
+ 
+        int wordCount = countWords(sentence);
+
+        System.out.println("The number of words in your sentence is: " + wordCount);
+    }
+}
